c7298696521a9a378c071ca460b24e410b2e4055
Olatunde Emmanuel <137836728+EmmanuelOlatunde@users.noreply.github.com>
2025-01-19 23:55:47 +0100

    Corrected and Improved settings

--- paytrack/settings.py
+++ paytrack/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-s7)^129ii*c=6fdz5it7ab+s$!t_4g!(cw%cx0ue(ym#2klqcx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", cast=bool)
+DEBUG = True
 
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = []
 
 # Application definition
 
