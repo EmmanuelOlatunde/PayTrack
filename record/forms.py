5e777418eaf89dc3bffb8d72ba781c42f3aa4ed4
Olatunde Emmanuel <137836728+EmmanuelOlatunde@users.noreply.github.com>
2025-01-25 11:42:21 +0100

    Update forms.py
    Made the monthly collection Form Requird

--- record/forms.py
+++ record/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
- 
+
 class MemberRegistrationForm(UserCreationForm):
-    monthly_target = forms.DecimalField(max_digits=10, initial=0)
+    monthly_target = forms.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        required=True,
+        initial=3000,
+        error_messages={'required': 'Please enter a monthly target.'}
+    )
 
     class Meta:
         model = User
